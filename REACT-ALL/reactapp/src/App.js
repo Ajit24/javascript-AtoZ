0dc5e43f16d61197697ec7d494fcce591534836b
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-07-19 00:09:38 +0530

    CRUD in reactjs

--- REACT-ALL/reactapp/src/App.js
+++ REACT-ALL/reactapp/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import HideAndShowPassword from './components/HideAndShowPassword/HideAndShowPassword';
-
+import CRUD from './components/CRUD/CRUD';
 function App() {
   return (
     <div className="App">
-      <HideAndShowPassword/>
+      {/* <HideAndShowPassword/> */}
+       <CRUD/>
     </div>
   );
 }
