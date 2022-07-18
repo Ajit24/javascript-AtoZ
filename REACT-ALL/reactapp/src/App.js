4c205c76c3fbf5134ef61f7f4f3904d871acffd8
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-07-19 00:26:49 +0530

    add dynamic input reactjs

--- REACT-ALL/reactapp/src/App.js
+++ REACT-ALL/reactapp/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import HideAndShowPassword from './components/HideAndShowPassword/HideAndShowPassword';
 import CRUD from './components/CRUD/CRUD';
+import AddDynamicInput from './components/AddDynamicInput/AddDynamicInput';
 function App() {
   return (
     <div className="App">
       {/* <HideAndShowPassword/> */}
-       <CRUD/>
+       {/* <CRUD/> */}
+       <AddDynamicInput/>
     </div>
   );
 }
