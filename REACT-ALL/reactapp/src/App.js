84d14f23e48006a158f84bbf86ed57d864712e7a
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-07-19 00:32:05 +0530

    get api call reactjs

--- REACT-ALL/reactapp/src/App.js
+++ REACT-ALL/reactapp/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import HideAndShowPassword from './components/HideAndShowPassword/HideAndShowPassword';
 import CRUD from './components/CRUD/CRUD';
 import AddDynamicInput from './components/AddDynamicInput/AddDynamicInput';
+import GetApiCall from './components/GetApiCall/GetApiCall';
 function App() {
   return (
     <div className="App">
       {/* <HideAndShowPassword/> */}
        {/* <CRUD/> */}
-       <AddDynamicInput/>
+       {/* <AddDynamicInput/> */}
+       <GetApiCall/>
     </div>
   );
 }
