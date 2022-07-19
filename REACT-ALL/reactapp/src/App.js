2691d09348fe815395eb17349b1aaeb85d4ef5e5
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-07-19 23:07:26 +0530

    Update App.js

--- REACT-ALL/reactapp/src/App.js
+++ REACT-ALL/reactapp/src/App.js
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App">
       {/* <HideAndShowPassword/> */}
-       {/* <CRUD/> */}
+        {/* <CRUD/> */}
        {/* <AddDynamicInput/> */}
-       <GetApiCall/>
+       {/* <GetApiCall/> */}
        {/* <ImageUpload/> */}
          
     
