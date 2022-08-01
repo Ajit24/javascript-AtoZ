1d8404dcdb07d2a9877634e1e01fae75284342e3
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-01 13:43:08 +0530

    context-api reactjs

--- REACT-INTQ/context-api/src/App.js
+++ REACT-INTQ/context-api/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import { UserData } from "./contexts/GlobalContext";
 
 function App() {
+  const [name, setName] = useState("Ajit");
+  const [age, setAge] = useState(25);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserData.Provider value={{ name, setName, age }}>
+        <Home />
+        <Profile />
+      </UserData.Provider>
     </div>
   );
 }
