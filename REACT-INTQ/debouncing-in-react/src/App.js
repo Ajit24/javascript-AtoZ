19bc1eb1d1e1dcb23b39f47163e5ea2eb01525ac
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-02 23:46:09 +0530

    debouncing-reactjs

--- REACT-INTQ/debouncing-in-react/src/App.js
+++ REACT-INTQ/debouncing-in-react/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+const pinAPI = `https://api.postalpincode.in/pincode/`;
 function App() {
+  const [pin, setPin] = useState("");
+  useEffect(() => {
+    const debouncing = setTimeout(() => {
+      axios
+        .get(pinAPI + pin)
+        .then((res) => {
+          console.log(res.data[0].PostOffice);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, 2000);
+
+    return () => {
+      clearTimeout(debouncing);
+    };
+  }, [pin]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={(event) => setPin(event.target.value)}
+        placeholder="Enter your Pin-Code"
+      />
     </div>
   );
 }
 
 export default App;
+
