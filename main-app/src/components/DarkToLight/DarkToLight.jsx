1aa58a67a8c9984045101de0a42c797e7dcb82ae
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-16 11:11:08 +0530

    DARK TO LIGHT THEME

--- main-app/src/components/DarkToLight/DarkToLight.jsx
+++ main-app/src/components/DarkToLight/DarkToLight.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import './DarkToLight.css';
+function DarkToLight(){
+    const [theme,setTheme]=useState(false);
 
-const DarkToLight = () => {
-  return (
-    <div>
-      <h1>DarkToLight</h1>
-    </div>
-  )
+    const handleClick=()=>{
+        setTheme(!theme)
+    }
+    useEffect(()=>{
+        if(theme==true){
+            document.body.classList.add("dark");
+        }else{
+            document.body.classList.remove("dark");
+        }
+    })
+
+    return(
+        <div>
+            <button onClick={handleClick}>{theme?"Light":"Dark"}</button>
+            <h1>Theme color in React</h1>
+            <span>Light and Dart theme</span>
+        </div>
+    );
 }
-
-export default DarkToLight
+export default DarkToLight;
